896a16bece18d60740d11324bcb1784830ad5bba
Steffen <24982394+Stev-eH@users.noreply.github.com>
2022-12-30 12:44:30 +0100

    Start Aufgabe 2

---
+++ include/CapycitySim.h
@@ -0,0 +1,21 @@
+#ifndef CAPYCITYSIM_H
+#define CAPYCITYSIM_H
+
+enum gebaeude { LEER = 0, WASSERKRAFTWERK = 1, WINDKRAFTWERK = 2, SOLARPANELE = 3 };
+
+class CapycitySim
+{
+    public:
+        gebaeude* area;
+        int x;
+        int y;
+
+        CapycitySim();
+        ~CapycitySim();
+        int getVar(int type, bool isX);
+        void drawField();
+        void showMenu();
+        void setBuilding(bool place);
+};
+
+#endif // CAPYCITYSIM_H
